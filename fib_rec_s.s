5d63c53fab1375ed7189ee8c2146768e0048e745
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-20 22:41:20 +0000

    fibrec_s imported from lab04

--- fib_rec_s.s
+++ fib_rec_s.s
@@ -1,5 +1,51 @@
 .global fib_rec_s
 
 fib_rec_s:
-    # YOUR CODE HERE
+	addi sp, sp, -80
+	sd ra, (sp)
+	
+	li t1, 1
+	bgt a0, t1, recstep
+	j done
+
+recstep:
+	addi t1, a0, -1
+
+	# preserve before call fibrec(n-1)
+	sd a0, 8(sp)
+	mv a0, t1
+	sd t0, 16(sp)
+	sd t1, 24(sp)
+	
+	call fib_rec_s
+	mv t2, a0
+	
+	# restore after call fibrec(n-1)
+	ld a0, 8(sp)
+	ld t0, 16(sp)
+	ld t1, 24(sp)
+	
+	addi t1, a0, -2
+	
+	# preserve before call fibrec(n-2)
+	sd a0, 8(sp)
+	mv a0, t1
+	sd t0, 16(sp)
+	sd t1, 24(sp)
+	sd t2, 32(sp)
+	
+	call fib_rec_s
+	mv t3, a0
+
+	# restore after call fibrec(n-2)
+	ld a0, 8(sp)
+	ld t0, 16(sp)
+	ld t1, 24(sp)
+	ld t2, 32(sp)
+	
+	add a0, t2, t3
+
+done:
+	ld ra, (sp)
+	addi sp, sp, 80
     ret
