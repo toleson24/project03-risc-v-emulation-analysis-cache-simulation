2baad6ca3b74e83c92149a53a54fd7e0615b1bd9
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-21 17:08:07 +0000

    code cleaned

--- bits.c
+++ bits.c
@@ -9,11 +9,6 @@ uint32_t get_bits(uint64_t num, uint32_t start, uint32_t count) {
 }
 
 int64_t sign_extend(uint64_t num, uint32_t start) {
-	/*int64_t signed_bit = (num >> (start - 1)) & 1;
-	int64_t sign_extended_num = signed_bit ? ~0 << start : 0; 
-		
-	return (int64_t) num | sign_extended_num;*/
-	
 	num = num << (64 - start);
 	int64_t x = ((int64_t) num >> (64 - start));
 	
